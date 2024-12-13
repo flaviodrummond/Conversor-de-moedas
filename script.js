f80358519f5af6acb419bc805bb2fec704ed4401
flaviodrummond <flaviosax20@gmail.com>
2024-12-13 02:54:20 -0300

    Criado o if para pegar o select do HTML e converter Dolar e Euro

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 const convertButton = document.querySelector(".convert-button")
 const valueConvert = document.querySelector(".value-convert")
 const valueConverted = document.querySelector(".converted-value")
+const selectConverted = document.querySelector(".select-converted")
+
 
 function convertValue() {
 
-    const dollarDay = 6.05
+    const dollarToDay = 6.05
+    const euroToDay = 6.27
 
     const inputValue = document.querySelector(".input-value").value
 
-    const convertedValue = inputValue / dollarDay
+    if(selectConverted.value == "dollar"){
+        valueConverted.innerHTML = new Intl.NumberFormat('en-US', {
+            style: "currency",
+            currency: "USD"
+        }).format(inputValue / dollarToDay)
+    }
+
+    if(selectConverted.value == "euro"){
+        valueConverted.innerHTML = new Intl.NumberFormat('de-DE', {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputValue / euroToDay)
+    }
 
     valueConvert.innerHTML = new Intl.NumberFormat('pt-BR', {
         style: "currency",
         currency: "BRL"
     }).format(inputValue)
 
-    valueConverted.innerHTML = new Intl.NumberFormat('en-US', {
-        style: "currency",
-        currency: "USD"
-    }).format(convertedValue)
+    }
+    
 
-}
+    
 
 convertButton.addEventListener("click", convertValue)
 
